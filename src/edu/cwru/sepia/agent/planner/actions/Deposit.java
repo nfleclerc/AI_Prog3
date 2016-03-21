876a7ecbaa09404331a269caf4ec582bb97c49df
Nathaniel Leclerc <nfleclerc@live.com>
2016-03-20 23:10:37 -0400

    created base classes for stripsactions
    Signed-off-by: Nathaniel Leclerc <nfleclerc@live.com>

---
+++ src/edu/cwru/sepia/agent/planner/actions/Deposit.java
@@ -0,0 +1,18 @@
+package edu.cwru.sepia.agent.planner.actions;
+
+import edu.cwru.sepia.agent.planner.GameState;
+
+/**
+ * Created by nathaniel on 3/20/16.
+ */
+public class Deposit implements StripsAction {
+    @Override
+    public boolean preconditionsMet(GameState state) {
+        return false;
+    }
+
+    @Override
+    public GameState apply(GameState state) {
+        return null;
+    }
+}
